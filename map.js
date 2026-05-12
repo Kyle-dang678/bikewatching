d93e86bbda8af7e304fc78ccc2b28570114a6b68
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-05-12 14:16:31 -0700

    Update map.js

--- map.js
+++ map.js
@@ -30,6 +30,43 @@ function minutesSinceMidnight(date) {
   return date.getHours() * 60 + date.getMinutes();
 }
 
+let departuresByMinute = Array.from({ length: 1440 }, () => []);
+let arrivalsByMinute = Array.from({ length: 1440 }, () => []);
+
+function filterByMinute(tripsByMinute, minute) {
+  if (minute === -1) return tripsByMinute.flat();
+  let minMinute = (minute - 60 + 1440) % 1440;
+  let maxMinute = (minute + 60) % 1440;
+  if (minMinute > maxMinute) {
+    return [
+      ...tripsByMinute.slice(minMinute),
+      ...tripsByMinute.slice(0, maxMinute),
+    ].flat();
+  } else {
+    return tripsByMinute.slice(minMinute, maxMinute).flat();
+  }
+}
+
+function computeStationTraffic(stations, timeFilter = -1) {
+  const departures = d3.rollup(
+    filterByMinute(departuresByMinute, timeFilter),
+    (v) => v.length,
+    (d) => d.start_station_id,
+  );
+  const arrivals = d3.rollup(
+    filterByMinute(arrivalsByMinute, timeFilter),
+    (v) => v.length,
+    (d) => d.end_station_id,
+  );
+  return stations.map((station) => {
+    let id = station.short_name;
+    station.arrivals = arrivals.get(id) ?? 0;
+    station.departures = departures.get(id) ?? 0;
+    station.totalTraffic = station.arrivals + station.departures;
+    return station;
+  });
+}
+
 map.on("load", async () => {
   map.addSource("boston_route", {
     type: "geojson",
@@ -80,6 +117,8 @@ map.on("load", async () => {
     (trip) => {
       trip.started_at = new Date(trip.started_at);
       trip.ended_at = new Date(trip.ended_at);
+      departuresByMinute[minutesSinceMidnight(trip.started_at)].push(trip);
+      arrivalsByMinute[minutesSinceMidnight(trip.ended_at)].push(trip);
       return trip;
     },
   );
@@ -96,13 +135,7 @@ map.on("load", async () => {
     (d) => d.end_station_id,
   );
 
-  stations = stations.map((station) => {
-    let id = station.short_name;
-    station.arrivals = arrivals.get(id) ?? 0;
-    station.departures = departures.get(id) ?? 0;
-    station.totalTraffic = station.arrivals + station.departures;
-    return station;
-  });
+  stations = computeStationTraffic(stations);
 
   const radiusScale = d3
     .scaleSqrt()
@@ -124,6 +157,15 @@ map.on("load", async () => {
           `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
         );
     });
+
+  function updateScatterPlot(timeFilter) {
+    const filteredStations = computeStationTraffic(stations, timeFilter);
+    timeFilter === -1 ? radiusScale.range([0, 25]) : radiusScale.range([3, 50]);
+    circles
+      .data(filteredStations, (d) => d.short_name)
+      .join("circle")
+      .attr("r", (d) => radiusScale(d.totalTraffic));
+  }
 
   function updatePositions() {
     circles
@@ -151,6 +193,7 @@ map.on("load", async () => {
       anyTimeLabel.style.display = "none";
     }
   }
+  updateScatterPlot(timeFilter);
   timeSlider.addEventListener("input", updateTimeDisplay);
   updateTimeDisplay();
 });
