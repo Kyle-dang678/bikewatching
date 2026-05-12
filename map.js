f0f3c3c3bd2f27aa2b69622949aa1c89069d4477
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-05-12 12:02:37 -0700

    Update map.js

--- map.js
+++ map.js
@@ -61,19 +61,56 @@ map.on("load", async () => {
   } catch (error) {
     console.error("Error loading JSON", error);
   }
+
   let stations = jsonData.data.stations;
   console.log("Stations Array:", stations);
+
   const svg = d3.select("#map").select("svg");
+
+  const radiusScale = d3
+    .scaleSqrt()
+    .domain([0, d3.max(stations, (d) => d.totalTraffic)])
+    .range([0, 25]);
+
   const circles = svg
     .selectAll("circle")
     .data(stations, (d) => d.short_name)
     .enter()
     .append("circle")
-    .attr("r", 5)
+    .attr("r", (d) => radiusScale(d.totalTraffic))
     .attr("fill", "steelblue")
     .attr("stroke", "white")
     .attr("stroke-width", 1)
     .attr("opacity", 0.8);
+
+  const trips = await d3.csv(
+    "https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv",
+    (trip) => {
+      trip.started_at = new Date(trip.started_at);
+      trip.ended_at = new Date(trip.ended_at);
+      return trip;
+    },
+  );
+
+  const departures = d3.rollup(
+    trips,
+    (v) => v.length,
+    (d) => d.start_station_id,
+  );
+
+  const arrivals = d3.rollup(
+    trips,
+    (v) => v.length,
+    (d) => d.end_station_id,
+  );
+
+  stations = stations.map((station) => {
+    let id = station.short_name;
+    station.arrivals = arrivals.get(id) ?? 0;
+    station.departures = departures.get(id) ?? 0;
+    station.totalTraffic = station.arrivals + station.departures;
+    return station;
+  });
 
   function updatePositions() {
     circles
