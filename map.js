e88ab41b546f454f0cd852cb4408ac9bdd38ac4d
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-05-12 11:06:14 -0700

    Added map.js

---
+++ map.js
@@ -0,0 +1,50 @@
+import mapboxgl from "https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm";
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+import MAPBOX_TOKEN from "./config.js";
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
+
+map.on("load", async () => {
+  // Boston bike lanes
+  map.addSource("boston_route", {
+    type: "geojson",
+    data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson",
+  });
+
+  map.addLayer({
+    id: "bike-lanes",
+    type: "line",
+    source: "boston_route",
+    paint: {
+      "line-color": "#32D400",
+      "line-width": 5,
+      "line-opacity": 0.6,
+    },
+  });
+
+  // Cambridge bike lanes
+  map.addSource("cambridge_route", {
+    type: "geojson",
+    data: "https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson",
+  });
+
+  map.addLayer({
+    id: "cambridge-bike-lanes",
+    type: "line",
+    source: "cambridge_route",
+    paint: {
+      "line-color": "#32D400",
+      "line-width": 5,
+      "line-opacity": 0.6,
+    },
+  });
+});
