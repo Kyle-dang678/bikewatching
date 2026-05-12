4df030750e533fe6b0b27f7e4e261e0fefd8fb53
Kyle Dang <127087021+Kyle-dang678@users.noreply.github.com>
2026-05-12 12:30:44 -0700

    Update map css and js

--- map.js
+++ map.js
@@ -106,10 +106,13 @@ map.on("load", async () => {
     .enter()
     .append("circle")
     .attr("r", (d) => radiusScale(d.totalTraffic))
-    .attr("fill", "steelblue")
-    .attr("stroke", "white")
-    .attr("stroke-width", 1)
-    .attr("opacity", 0.8);
+    .each(function (d) {
+      d3.select(this)
+        .append("title")
+        .text(
+          `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`,
+        );
+    });
 
   function updatePositions() {
     circles
